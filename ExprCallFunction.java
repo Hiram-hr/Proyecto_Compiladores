a4b17b69ab169ad780474bd73f962bb19ea07f20
Hiram-hr <118991748+Hiram-hr@users.noreply.github.com>
2024-01-16 21:58:03 -0600

    Mi parte

--- ExprCallFunction.java
+++ ExprCallFunction.java
@@ -1,11 +1,50 @@
+
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class ExprCallFunction extends Expression{
     final Expression callee;
+    // final Token paren;
     final List<Expression> arguments;
 
-    ExprCallFunction(Expression callee, List<Expression> arguments){
-        this.callee=callee;
-        this.arguments=arguments;
+    ExprCallFunction(Expression callee, /*Token paren,*/ List<Expression> arguments) {
+        this.callee = callee;
+        // this.paren = paren;
+        this.arguments = arguments;
+    }
+
+    @Override
+    public Object solve(TablaSimbolos tablaSimbolos) {
+
+        TablaSimbolos tabla_nueva = new TablaSimbolos(tablaSimbolos); 
+
+        // Verificamos que no sea una expresión de tipo variable
+        if(!(callee instanceof ExprVariable))
+            throw new RuntimeException("Error al invocar función");
+
+        Object funcion_temp = callee.solve(tablaSimbolos);
+        if(!(funcion_temp instanceof StmtFunction))
+            throw new RuntimeException("Error: Identificador inválido para invocar función");
+
+       
+        // Una vez verificado que sea una función, obtenemos los argumentos de la función y los resolvemmos
+        List<Object> argumentos = new ArrayList<>();
+
+        for(Expression argumento: arguments)
+            argumentos.add(argumento.solve(tabla_nueva));
+
+        // Verificamos si se pudieron resolver todos los argumentos
+        if(argumentos.size() != ((StmtFunction) funcion_temp).params.size())
+            throw new RuntimeException("Error: Error en argumentos al invocar "+ ((ExprVariable) callee).name.lexema);
+
+        int i = 0;
+        for(Token token: ((StmtFunction) funcion_temp).params){
+
+            tabla_nueva.asignar(token.lexema, argumentos.get(i)); 
+            i++;
+        }
+
+        return ((StmtFunction) funcion_temp).body.execute(tabla_nueva);
     }
 }
--- StmtBlock.java
+++ StmtBlock.java
@@ -1,3 +1,4 @@
+
 import java.util.List;
 
 public class StmtBlock extends Statement{
@@ -5,5 +6,25 @@ public class StmtBlock extends Statement{
 
     StmtBlock(List<Statement> statements) {
         this.statements = statements;
+    }
+
+    @Override
+    public Object execute(TablaSimbolos tablaSimbolos) {
+
+        TablaSimbolos tabla_nueva = new TablaSimbolos(tablaSimbolos);
+
+        // Ejecutamos todas las declaraciones de la lista
+        for(Statement statement: statements){
+
+            if(statement != null){
+
+                if(statement instanceof StmtReturn)
+                    return statement.execute(tabla_nueva);
+
+                statement.execute(tabla_nueva);
+            }
+        }
+
+        return null;
     }
 }
